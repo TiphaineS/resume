f6908c309221bf9f0096768b81bc112309c51851
HeadlessCell <62139898+TiphaineS@users.noreply.github.com>
2026-09-03 21:52:11 +0200

    Update resume-config.ts

--- src/data/resume-config.ts
+++ src/data/resume-config.ts
@@ -187,7 +187,7 @@ export const resumeConfig: ResumeConfig = {
       title: { fr: 'Snowboard', en: 'Snowboard' },
     },
   ],
-  theme: { preset: 'retro' },
+  theme: { preset: 'sepia' },
   labels: {
     sections: {
       contact: { fr: 'CONTACT', en: 'CONTACT' },
